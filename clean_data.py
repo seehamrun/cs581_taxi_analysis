88abf0281f77267e7903e593768ceb57de445dd2
seehamrun <sihamh@google.com>
2020-02-03 20:13:43 -0600

    write out cleaned csv

--- clean_data.py
+++ clean_data.py
@@ -1,16 +1,22 @@
 import csv
 
+valid_trips = []
+
 with open('data/yellow_tripdata_2019-01.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
-    valid_trips = []
 
     for row in csv_reader:
-        #     print(str(row["PULocationID"]) + " " + str(row["DOLocationID"]) + " " + str(row["passenger_count"]))
         if (int(row["PULocationID"]) == 138 or int(row["DOLocationID"]) == 138):
         	valid_trips.append(row)
-
         line_count += 1
 
     print('Processed ' + str(line_count) + ' lines.')    
     print('Kept ' + str(len(valid_trips)) + ' lines.')
+
+    keys = valid_trips[0].keys()
+
+with open('data/clean.csv', 'wb') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(valid_trips)
